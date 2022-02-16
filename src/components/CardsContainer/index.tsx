6a7e5e1de6de18b9bcec1348ec26190954d654a8
Hoshibi <tanyshiotani@hotmail.com>
2022-02-16 11:19:41 -0300

    Exotic_Cars - Component CardsContainer
    - Criação do component CardContainer
    - Estilização do component CardContainer
    - Utilização do axios para pegar os dados do arquivo json
    - Importação dos components Cards com as informações obtidas

---
+++ src/components/CardsContainer/index.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback, useEffect, useState } from "react";
+import axios from 'axios';
+
+import Card from "@components/Card";
+import { Container } from "./styles";
+
+const CardsContainer: React.FC = () => {
+    const [infoCar, getInfoCar] = useState([]);
+    
+    const getCar = useCallback(async () => {
+        axios.get('./cars.json')
+          .then(function (response) {
+            getInfoCar(response.data.cars);
+          })
+          .catch(function (error) {
+            console.log(error);
+        })
+    }, []);
+
+    useEffect(() => {
+        getCar();
+    }, [getCar]);
+
+    return (
+        <Container>
+            {infoCar.map((info,index) => {
+                console.log(info)
+                return <Card key={index} brand={info["brand"]} model={info["model"]} price={info["price"]} imagem={info["sideImage"]}/>
+            })}
+        </Container>
+    );
+}
+
+export default CardsContainer;
---
+++ src/components/CardsContainer/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+    margin: 102px 80px 57px 90px;
+    display: flex;
+    flex-wrap: wrap;
+;`
